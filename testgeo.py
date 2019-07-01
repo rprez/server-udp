4e0988313e33c7c086b36c8f356e79c3545e6417
Rodrigo Pérez <rgperez@antel.com.uy>
2019-07-01 15:19:49 -0300

    Quito archivos innecesarios

--- testgeo.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-location = geolocator.geocode(" pedro vidal 2471  ,Montevideo")
-print(location.address)
-print((location.latitude, location.longitude))
-print(location.raw)
