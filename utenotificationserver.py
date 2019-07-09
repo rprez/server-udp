ba39bd40c910387ecdf20f81a99ac45fb822e0db
Rodrigo Pérez <rgperez@antel.com.uy>
2019-07-09 15:12:03 -0300

    Cambio el nombre al principal

--- utenotificationserver.py
+++ utenotificationserver.py
@@ -1,15 +1,10 @@
 import json
-import socket
 import threading
 from datetime import datetime
-import queue
 import time
 import os
 import shutil
-import sys
 from NotificationDBStorageAlchemy import  NotificationDatabase
-from uteUdpNotificationReceiver import UteUdpReceiver
-
 
 def parseDate(dir):
     
@@ -223,7 +218,7 @@ class SocketHandler_NotificationHistory (threading.Thread):
     
         return [None,0]   
                
-    def checkIfRequestPending(self,sockDict):
+    def check_IfRequestPending(self,sockDict):
         
         if not ("stream" in sockDict):
             sockDict['stream']=""
@@ -342,7 +337,7 @@ class SocketHandler_NotificationHistory (threading.Thread):
         return result
                 
     
-    def run(self):     
+    def run(self):
     
         while True:            
         
@@ -361,102 +356,6 @@ class SocketHandler_NotificationHistory (threading.Thread):
                     #handle request.....!!!
                     
             
-            time.sleep(0.5)   
+            time.sleep(0.5)
        
-        
-if __name__ == "__main__":
-    HOST, PORT = "", 15353   
-    udpPort = 12345
-   
-    if not os.path.exists("history"):
-        os.makedirs("history")
-  
-    storageMode="db"
-    if len(sys.argv)==2:
-        if sys.argv[1]=="-filedb":
-            storageMode="file"
-        
-    
-    storageQueue=queue.Queue()
-    storageQueueLock=threading.Lock()
-    config={}
-    dbHandler=None
-    
-    if storageMode=="db":
-        try:
-            config=json.load(open("udpserverconfig.json"))
-          
-        except Exception as e:
-            print("Error al cargar configuracion")
-            print(e)  
-            exit(0)
-        
-        try:  
-            dbHandler=StorageDbHandler(config, storageQueue, storageQueueLock)
-            dbHandler.setDaemon(True)
-            dbHandler.start()    
-        except Exception as e:
-            print("Error al conectar con base de datos")
-            print(e)  
-            exit(0)
-        
-            
-      
-   # udpSimulator=UteUdpSimulator("localhost",12345,0.1)
-  #  udpSimulator.setDaemon(True)
-  #  udpSimulator.start()
-   
-   # #notification receiver
-    udpQueue = queue.Queue()
-    udpLock = threading.Lock()
-    udpReceiver = UteUdpReceiver(udpPort, udpQueue, udpLock)
-    udpReceiver.setDaemon(True)
-    udpReceiver.start()
-    
-    
 
-    
-   # #notification distributor
-    newSocketQueue = queue.Queue()
-    newSocketLock = threading.Lock()
-    
-    
-    resender = SocketHandler_NotificationDistributor(newSocketQueue, newSocketLock, udpQueue, udpLock, storageQueue,storageQueueLock,storageMode)
-    resender.setDaemon(True)
-    resender.start()
-    
-    # conection receiver
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(20)
-    
-    
-    
-    historyHandler=SocketHandler_NotificationHistory(resender.socketList)
-    historyHandler.setDaemon(True)
-    historyHandler.start()
-    
-    historyCleaner=HistoryCleaner(30)
-    historyCleaner.setDaemon(True)
-    historyCleaner.start()
-    
-    
-    while True:
-
-        print("Waiting connection")
-        print("Active Thread Count Before enviarFile: %d" % (threading.activeCount()))
-        client, fromaddr = server_socket.accept()                
-         
-        newSocketLock.acquire()
-        newLock=threading.Lock()
-        newDict={}
-        newDict['socket']=client
-        newDict['lock']=newLock
-        newSocketQueue.put(newDict)
-        newSocketLock.release()
-        print("Connection accepted from ")
-        print(fromaddr)
-        ip = fromaddr[0]
-       
-    
